2aadd127af264a6b32bd3c022faa0267e1f5d458
MainTeraHer0 <72750605+MainTeraHer0@users.noreply.github.com>
2020-11-10 12:05:34 +0530

    Update events.py

--- ionix/events.py
+++ ionix/events.py
@@ -15,7 +15,7 @@ from traceback import format_exc
 
 from telethon import events
 
-from userbot import bot, BOTLOG_CHATID, LOGSPAMMER
+from ionix import bot, BOTLOG_CHATID, LOGSPAMMER
 
 
 def register(**args):
